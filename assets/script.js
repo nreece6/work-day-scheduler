ad7c8e45b628a1da37e4e79f211acc21c7641a2e
nreece6 <reece.nick24@gmail.com>
2023-04-09 14:06:43 -0500

    updated js for event listeners, local storage, and built out main function to determine current time vs id

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,62 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+var today = dayjs()
+var calendarBlockEl = document.querySelector('.container')
+
+
+// Display Current Day/Time Refreshing every second
+setInterval(function() {
+  $('#currentDay').text(dayjs().format('[Today is ] MMM D, YYYY'))
+  $('#currentTime').text(dayjs().format('[The time is ] hh:mm:ss'))
+  ,1000})
+
+  //Listener for save button click - uses jQuery to get nearby values, grab parent ID and save to localStorage
+  $('.saveBtn').on('click', function() {
+    var textValue = $(this).siblings('.description').val()
+    var timeValue = $(this).parent().attr('id')
+    localStorage.setItem(timeValue, textValue)
+  })
+
+  // gets values for each hour from local storage
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'));
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  //main function to determine current time and switch classes depending on relation to current time
+function checkTime() {
+  var currentTime = today.format('H')
+  console.log(currentTime)
+
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split("hour-")[1]);
+    console.log(timeId)
+
+    // if the time Id attribute is before the current hour, add the past class
+    if (timeId < currentTime) {
+      $(this).addClass('past');
+    } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
+    else if (timeId == currentTime) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+})
+}
+
+checkTime()
+
+
+ 
